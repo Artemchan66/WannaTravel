d286e80bbd2eb507e32eaabecbf1359f7089be69
Artemchan66 <artemchan@gmail.com>
2026-01-08 18:13:54 +0300

    put map load to backend, created controller
    make map appear in front

---
+++ back/api/Controllers/MapController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WannaTravel.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MapController : ControllerBase
+{
+    [HttpGet("world")]
+    public IActionResult GetWorldMap()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "world.svg");
+        var svg = System.IO.File.ReadAllText(path);
+        return Content(svg, "image/svg+xml");
+    }
+}
--- back/api/Program.cs
+++ back/api/Program.cs
@@ -5,7 +5,16 @@ using WannaTravel.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options 
+    => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserLogic, UserLogic>();
@@ -27,6 +36,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
